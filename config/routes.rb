10d143019823f018a96212607e8ae983c568843a
Emily Ellison <emilyellison986@gmail.com>
2012-04-02 17:01:43 -0500

    Finish static pages v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 TheKordtCo::Application.routes.draw do
+  
+  root to: "static_pages#home"
+
+  match "/about", to: "static_pages#about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
